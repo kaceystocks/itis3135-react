2a5b502e0e4deec96695651b115b94254f9a699c
kstocks3 <kstocks3@charlotte.edu>
2025-11-12 13:09:20 -0500

    .

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+export default function Header() {
+    return <header>
+<h1>Kacey Stocks Knowledgable Seal || ITIS3135</h1>
+        <nav>
+            <a href="./">Home</a>
+            ||
+            <a href="introduction.html">Introduction</a>
+            ||
+            <a href="contract.html">Contract</a>
+            
+        </nav>
+        <br></br>
+        <nav>
+            <a href="../itis3135/mascotcompany/index.html">Mascot Company</a> || 
+            <a href="../itis3135/stuff/CRAP!$web site.htm">CRAPPY Webpage</a> ||
+            <a href="../itis3135/hobby/index.html">Hobby</a> ||
+            <a href="../itis3135/project/index.html">Client Project</a>
+        </nav>
+    </header>
+}
