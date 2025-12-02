e4f4c4555b98ffbf583d8c7331015eab27e7885b
kstocks3 <kstocks3@charlotte.edu>
2025-12-02 17:29:13 -0500

    router

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import Introductions from './Introductions.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<Layout/>}>
           <Route path='/' element={<App/>}></Route>
@@ -22,6 +22,6 @@ createRoot(document.getElementById('root')).render(
         </Route>
 
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
